26046d65b7200c998f6d965431f1b2c84f8c6ae5
zhenshan <zhenshan@belmont.tech>
2022-02-17 10:06:57 +0800

    FIX comments

--- main.py
+++ main.py
@@ -5,27 +5,9 @@ from email.mime.text import MIMEText
 # For gmail use, go to the account settings 
 # and allow less secure apps to access the account:
 # https://myaccount.google.com/lesssecureapps
-
+# https://accounts.google.com/b/0/DisplayUnlockCaptcha
 
 def send_email(user:str, password:str, to:str, subject:str="", body:str="", smtp_server_host="smtp.gmail.com"):
-    # to = [to]
-    # email_text = f"""\
-    # From: {user}
-    # To: {", ".join(to)}
-    # Subject: {subject}
-
-    # {body}
-    # """
-    
-    # print("sending email: ")
-    # print(email_text)
-
-    # server = smtplib.SMTP(smtp_server_host, 587)
-    # server.starttls()
-    # server.login(user, password)
-    # server.sendmail(user, to, email_text)
-    # server.quit()
-
     try:
         connection = smtplib.SMTP(host=smtp_server_host, port=587)
         connection.starttls()
